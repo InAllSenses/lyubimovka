4f7f73f15ce51fd9efeae268d3ba784e330127e7
ElenaK <kinder.bubnova@yandex.ru>
2022-12-12 16:35:12 +0500

    JS slider-right for gallery

--- index.js
+++ index.js
@@ -1,22 +1,73 @@
 const images = document.querySelectorAll('.gallery__image');
-const closeButton = document.querySelector('.popup-image__close');
-const popup = document.querySelector('.popup-image');
-const image = document.querySelector('.popup-image__content')
+const imagePopup = document.querySelector('.popup-image');
+const popupImageElement = imagePopup.querySelector('.popup-image__content');
+const perfomancePopup = document.querySelector('.popup-perfomance');
+
+let currentImage = null;
 
 
-images.forEach(image => {
-    image.addEventListener('click', evt => {
-        console.log(evt);
-        popup.classList.add('popup-image_visible');
-    });
-});
+function showPopup(popupElement) {
+  popupElement.classList.add('popup_visible');
+}
 
+function closePopup(popupElement) {
+  popupElement.classList.remove('popup_visible');
+}
 
-function closePopup() {
-  closeButton.addEventListener("click", function () {
-    popup.classList.remove("popup-image_visible");
+function getCurrentIndex() {
+  for (let i = 0; i < images.length; i++) {
+    if (images[i] === currentImage)
+      return i;
+  }
+
+  return 0;
+}
+
+function getNextIndex() {
+  const currentIndex = getCurrentIndex();
+  let nextIndex = currentIndex+1;
+  if (nextIndex === images.length)
+    nextIndex = 0;
+
+  return nextIndex;
+}
+
+function getNextImage() {
+  const nextIndex = getNextIndex();
+  return images[nextIndex];
+}
+
+function switchImageToNext() {
+  const nextImage = getNextImage();
+  popupImageElement.src = nextImage.src;
+  currentImage = nextImage;
+}
+
+function addEventListenerArrowRight() {
+  const popupArrowRightButton = document.querySelector('.popup-image__arrow')
+
+  popupArrowRightButton.addEventListener("click", () => {
+    switchImageToNext();
   });
 }
 
-closePopup();
+function addEventListenerClose(popupElement) {
+  const popupCloseButton = popupElement.querySelector('.popup_close');
 
+  popupCloseButton.addEventListener("click", () => {
+    closePopup(popupElement);
+  });
+}
+
+images.forEach(image => {
+  image.addEventListener('click', evt => {
+      popupImageElement.src = image.src;
+      showPopup(imagePopup);
+      currentImage = image;
+  });
+});
+
+
+addEventListenerClose(imagePopup);
+addEventListenerClose(perfomancePopup);
+addEventListenerArrowRight(imagePopup);
